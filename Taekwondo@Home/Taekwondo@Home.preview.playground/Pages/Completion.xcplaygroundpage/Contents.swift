ae41bf825679648ef2a90b958e312bd5c9dca1ef
Jaesung <jaesung.lee@sendbird.com>
2020-05-22 01:52:41 +0900

    Update content
    🙃

--- Taekwondo@Home/Taekwondo@Home.preview.playground/Pages/Completion.xcplaygroundpage/Contents.swift
+++ Taekwondo@Home/Taekwondo@Home.preview.playground/Pages/Completion.xcplaygroundpage/Contents.swift
@@ -7,9 +7,6 @@
  
  You discovered what Taekwondo is and learnt some famous postures in 3D. Have you enjoyed my playground? I hope so!
  
- Taekwondo has different colors of belt based on the experience. Every beginner receives a white colored belt.
- Since you started Taekwondo, you deserve a white belt.
- 
  - Important:
  Don't stop here! Keep learning Taekwondo. There are some website to help you learn.
  
